feeb48e94ac56f5f0b681ade4db744c8b4472be8
Nishat Tasnim Ali <nishattasnimali3@gmail.com>
2026-03-05 22:18:58 +0600

    Initialize React project and install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXTwitter, faLinkedinIn, faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+
+
+
+  return (
+    <>
+      
+    </>
+  );
